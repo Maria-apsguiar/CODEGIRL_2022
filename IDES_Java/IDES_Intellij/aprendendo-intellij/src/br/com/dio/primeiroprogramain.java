080bc13a3a4a39090c7cf43a636f1c5a3aa179b0
Maria-apsguiar <cida.aguiar@outlook.com>
2022-08-09 19:56:21 -0300

    cria pasta curso JAVA

--- IDES_Java/IDES_Intellij/aprendendo-intellij/src/br/com/dio/primeiroprogramain.java
+++ IDES_Java/IDES_Intellij/aprendendo-intellij/src/br/com/dio/primeiroprogramain.java
@@ -1,2 +1,20 @@
-package br.com.dio;public class primeiroprogramain {
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+import java.sql.SQLOutput;
+
+public class primeiroprogramain {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+        System.out.println(gato);
+
+        Livro livro1 = new Livro ("O problema dos 3 corpos.", 300);
+        System.out.println(livro1);
+
+
+/*        int a = 5;
+        int b = 3;
+                System.out.println("Hello World!" + (a+b));*/
+    }
 }
