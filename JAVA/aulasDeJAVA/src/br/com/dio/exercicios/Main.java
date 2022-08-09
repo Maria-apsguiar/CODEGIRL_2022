e3c004e1e723ab748aac5586b2c455d641f55fa9
Maria-apsguiar <cida.aguiar@outlook.com>
2022-08-09 20:43:33 -0300

    Inclusão aula 1 JAVA

---
+++ JAVA/aulasDeJAVA/src/br/com/dio/exercicios/Main.java
@@ -0,0 +1,26 @@
+package br.com.dio.exercicios;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        int i; /*certo*/
+        /*    int i; variável igual a anterior, não podemos usar o mesmo nome para variáveis diferente*/
+        int I; /*case sensitive, faz diferenciação entre letras minusculas e letras maiusculas*/
+        /* int 1; váriaveis não podem começar com numeros*/
+        /* int _1a;
+        int &1a;
+        não se deve usar _ nem $ no nome das variáveis, devido as boas práticas*/
+
+        final int j = 10; /* variavel constante, não é possivel mudar este valor, devido ter usado final*/
+        int uahdsushdauhas; /*não é uma boa prática, por mais que possa usar letras e numeros para nomear a variavel, devo deixa-la de modo mais claro e objetivo e expressivo.*/
+
+        /* int a%mr; não posso usar % na nomeação de uma váriavel*/
+
+        int quantidadeDeProduto; /*boa prática*/
+        int QuantidadeDeProduto; /*Não é uma noa prática iniciar com letra maiscula*/
+
+        
+
+    }
+}
