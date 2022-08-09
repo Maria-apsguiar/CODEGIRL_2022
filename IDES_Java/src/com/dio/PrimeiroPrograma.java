9fd343dfafc8c7ad7591ed4a440dcecf784a6a73
Maria-apsguiar <cida.aguiar@outlook.com>
2022-08-09 17:16:38 -0300

    Cria classes em java e anotações do IDE Eclipse

--- IDES_Java/src/com/dio/PrimeiroPrograma.java
+++ IDES_Java/src/com/dio/PrimeiroPrograma.java
@@ -1,11 +1,19 @@
 package com.dio;
 
+import com.dio.model.Gato;
+
 public class PrimeiroPrograma {
 
 	public static void main(String[] args) {	
-		int a = 1;
+		
+		Gato gato = new Gato();
+		
+		
+		
+		/*int a = 1;
 		int b = 3;
-		System.out.println("Hello World! " + (a+b));
+		System.out.println("Hello World! " + (a+b));*/
+		
 	}
 
 }
